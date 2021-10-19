17dfc040cd90e86d28d3884ffe2577941264055b
Soufiane Hmiad <soufiane.hmiad@outlook.com>
2021-10-19 03:42:48 +0100

    export to excel + eiting filters

--- app.py
+++ app.py
@@ -1,42 +1,43 @@
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-import time
+from selenium.webdriver.support import expected_conditions as EC
+from bs4 import BeautifulSoup
+from time import sleep
+from selenium.webdriver.common.keys import Keys
 
 
-driver = webdriver.Firefox(executable_path="C:\\Users\\soufi\\Documents\\repositories\\Destination-Insights-with-Google\\webdrivers\\geckodriver.exe")
+
+filepath = (r"google-insights.xlsx")
+driver = webdriver.Firefox(executable_path=r"webdrivers\\geckodriver.exe")
 driver.get("https://destinationinsights.withgoogle.com/")
-
-
-assert "Destination Insights with Google" in driver.title
-
+#listcountries = [{"name":"Worldwide","id":"WW"},{"name":"Afghanistan","id":"AF"},{"name":"Albania","id":"AL"},{"name":"Algeria","id":"DZ"},{"name":"\u00c5land Islands","id":"AX"},{"name":"American Samoa","id":"AS"},{"name":"Andorra","id":"AD"},{"name":"Angola","id":"AO"},{"name":"Anguilla","id":"AI"},{"name":"Antarctica","id":"AQ"},{"name":"Antigua and Barbuda","id":"AG"},{"name":"Argentina","id":"AR"},{"name":"Armenia","id":"AM"},{"name":"Aruba","id":"AW"},{"name":"Australia","id":"AU"}, {"name":"Austria","id":"AT"},{"name":"Azerbaijan","id":"AZ"},{"name":"Bahrain","id":"BH"},{"name":"Bangladesh","id":"BD"},{"name":"Barbados","id":"BB"},{"name":"Belarus","id":"BY"},{"name":"Belgium","id":"BE"},{"name":"Belize","id":"BZ"},{"name":"Benin","id":"BJ"},{"name":"Bermuda","id":"BM"},{"name":"Bhutan","id":"BT"},{"name":"Bolivia","id":"BO"},{"name":"Bosnia and Herzegovina","id":"BA"},{"name":"Botswana","id":"BW"},{"name":"Bouvet Island","id":"BV"},{"name":"Brazil","id":"BR"},{"name":"British Indian Ocean Territory","id":"IO"},{"name":"British Virgin Islands", "id":"VG"},{"name":"Brunei","id":"BN"},{"name":"Bulgaria","id":"BG"},{"name":"Burkina Faso","id":"BF"},{"name":"Burundi","id":"BI"},{"name":"Cambodia","id":"KH"},{"name":"Cameroon","id":"CM"},{"name":"Canada","id":"CA"},{"name":"Cape Verde","id":"CV"},{"name":"Caribbean Netherlands","id":"BQ"},{"name":"Cayman Islands","id":"KY"},{"name":"Central African Republic","id":"CF"},{"name":"Chad","id":"TD"},{"name":"Chile","id":"CL"},{"name":"China","id":"CN"},{"name":"Christmas Island","id":"CX"},{"name":"Cocos (Keeling) Islands","id":"CC"},{"name":"Colombia","id":"CO"},{"name":"Comoros", "id":"KM"},{"name":"Cook Islands","id":"CK"},{"name":"Costa Rica","id":"CR"},{"name":"C\u00f4te d'Ivoire","id":"CI"},{"name":"Croatia","id":"HR"},{"name":"Cuba","id":"CU"},{"name":"Cura\u00e7ao","id":"CW"},{"name":"Cyprus","id":"CY"},{"name":"Czechia","id":"CZ"},{"name":"Democratic Republic of the Congo","id":"CD"},{"name":"Denmark","id":"DK"},{"name":"Djibouti","id":"DJ"},{"name":"Dominica","id":"DM"},{"name":"Dominican Republic","id":"DO"},{"name":"Ecuador","id":"EC"},{"name":"Egypt","id":"EG"},{"name":"El Salvador","id":"SV"},{"name":"Equatorial Guinea","id":"GQ"}, {"name":"Eritrea","id":"ER"},{"name":"Estonia","id":"EE"},{"name":"Eswatini","id":"SZ"},{"name":"Ethiopia","id":"ET"},{"name":"Falkland Islands (Islas Malvinas)","id":"FK"},{"name":"Faroe Islands","id":"FO"},{"name":"Federated States of Micronesia","id":"FM"},{"name":"Fiji","id":"FJ"},{"name":"Finland","id":"FI"},{"name":"France","id":"FR"},{"name":"French Guiana","id":"GF"},{"name":"French Polynesia","id":"PF"},{"name":"French Southern Territories","id":"TF"},{"name":"Gabon","id":"GA"},{"name":"Georgia","id":"GE"},{"name":"Germany","id":"DE"}, {"name":"Ghana","id":"GH"},{"name":"Gibraltar","id":"GI"},{"name":"Greece","id":"GR"},{"name":"Greenland","id":"GL"},{"name":"Grenada","id":"GD"},{"name":"Guadeloupe","id":"GP"},{"name":"Guam","id":"GU"},{"name":"Guatemala","id":"GT"},{"name":"Guernsey","id":"GG"},{"name":"Guinea-Bissau","id":"GW"},{"name":"Guinea","id":"GN"},{"name":"Guyana","id":"GY"},{"name":"Haiti","id":"HT"},{"name":"Honduras","id":"HN"},{"name":"Hong Kong","id":"HK"},{"name":"Hungary","id":"HU"},{"name":"Iceland","id":"IS"},{"name":"India","id":"IN"},{"name":"Indonesia","id":"ID"},{"name":"Iran","id":"IR"}, {"name":"Iraq","id":"IQ"},{"name":"Ireland","id":"IE"},{"name":"Isle of Man","id":"IM"},{"name":"Israel","id":"IL"},{"name":"Italy","id":"IT"},{"name":"Jamaica","id":"JM"},{"name":"Japan","id":"JP"},{"name":"Jersey","id":"JE"},{"name":"Jordan","id":"JO"},{"name":"Kazakhstan","id":"KZ"},{"name":"Kenya","id":"KE"},{"name":"Kiribati","id":"KI"},{"name":"Kuwait","id":"KW"},{"name":"Kyrgyzstan","id":"KG"},{"name":"Laos","id":"LA"},{"name":"Latvia","id":"LV"},{"name":"Lebanon","id":"LB"},{"name":"Lesotho","id":"LS"},{"name":"Liberia","id":"LR"},{"name":"Libya","id":"LY"},{"name":"Liechtenstein", "id":"LI"},{"name":"Lithuania","id":"LT"},{"name":"Luxembourg","id":"LU"},{"name":"Macao","id":"MO"},{"name":"Madagascar","id":"MG"},{"name":"Malawi","id":"MW"},{"name":"Malaysia","id":"MY"},{"name":"Maldives","id":"MV"},{"name":"Mali","id":"ML"},{"name":"Malta","id":"MT"},{"name":"Marshall Islands","id":"MH"},{"name":"Martinique","id":"MQ"},{"name":"Mauritania","id":"MR"},{"name":"Mauritius","id":"MU"},{"name":"Mayotte","id":"YT"},{"name":"Mexico","id":"MX"},{"name":"Moldova","id":"MD"},{"name":"Monaco","id":"MC"},{"name":"Mongolia", "id":"MN"},{"name":"Montenegro","id":"ME"},{"name":"Montserrat","id":"MS"},{"name":"Morocco","id":"MA"},{"name":"Mozambique","id":"MZ"},{"name":"Myanmar (Burma)","id":"MM"},{"name":"Namibia","id":"NA"},{"name":"Nauru","id":"NR"},{"name":"Nepal","id":"NP"},{"name":"Netherlands","id":"NL"},{"name":"New Caledonia","id":"NC"},{"name":"New Zealand","id":"NZ"},{"name":"Nicaragua","id":"NI"},{"name":"Niger","id":"NE"},{"name":"Nigeria","id":"NG"},{"name":"Niue","id":"NU"},{"name":"Norfolk Island","id":"NF"},{"name":"North Korea","id":"KP"},{"name":"North Macedonia", "id":"MK"},{"name":"Northern Mariana Islands","id":"MP"},{"name":"Norway","id":"NO"},{"name":"Oman","id":"OM"},{"name":"Pakistan","id":"PK"},{"name":"Palau","id":"PW"},{"name":"Palestinian Territory","id":"PS"},{"name":"Panama","id":"PA"},{"name":"Papua New Guinea","id":"PG"},{"name":"Paraguay","id":"PY"},{"name":"Peru","id":"PE"},{"name":"Philippines","id":"PH"},{"name":"Pitcairn","id":"PN"},{"name":"Poland","id":"PL"},{"name":"Portugal","id":"PT"},{"name":"Puerto Rico","id":"PR"},{"name":"Qatar","id":"QA"},{"name":"Republic of the Congo","id":"CG"},{"name":"R\u00e9union", "id":"RE"},{"name":"Romania","id":"RO"},{"name":"Russia","id":"RU"},{"name":"Rwanda","id":"RW"},{"name":"Saint Barthelemy","id":"BL"},{"name":"Saint Helena, Ascension and Tristan da Cunha","id":"SH"},{"name":"Saint Kitts and Nevis","id":"KN"},{"name":"Saint Lucia","id":"LC"},{"name":"Saint Martin","id":"MF"},{"name":"Saint Pierre and Miquelon","id":"PM"},{"name":"Saint Vincent and the Grenadines","id":"VC"},{"name":"Samoa","id":"WS"},{"name":"San Marino","id":"SM"},{"name":"S\u00e3o Tom\u00e9 and Pr\u00edncipe", "id":"ST"},{"name":"Saudi Arabia","id":"SA"},{"name":"Senegal","id":"SN"},{"name":"Serbia","id":"RS"},{"name":"Seychelles","id":"SC"},{"name":"Sierra Leone","id":"SL"},{"name":"Singapore","id":"SG"},{"name":"Sint Maarten","id":"SX"},{"name":"Slovakia","id":"SK"},{"name":"Slovenia","id":"SI"},{"name":"Solomon Islands","id":"SB"},{"name":"Somalia","id":"SO"},{"name":"South Africa","id":"ZA"},{"name":"South Korea","id":"KR"},{"name":"South Sudan","id":"SS"},{"name":"Spain","id":"ES"},{"name":"Sri Lanka","id":"LK"},{"name":"Sudan","id":"SD"},{"name":"Suriname","id":"SR"},{"name":"Svalbard and Jan Mayen", "id":"SJ"},{"name":"Sweden","id":"SE"},{"name":"Switzerland","id":"CH"},{"name":"Syria","id":"SY"},{"name":"Taiwan","id":"TW"},{"name":"Tajikistan","id":"TJ"},{"name":"Tanzania","id":"TZ"},{"name":"Thailand","id":"TH"},{"name":"The Bahamas","id":"BS"},{"name":"The Gambia","id":"GM"},{"name":"Timor-Leste","id":"TL"},{"name":"Togo","id":"TG"},{"name":"Tokelau","id":"TK"},{"name":"Tonga","id":"TO"},{"name":"Trinidad and Tobago","id":"TT"},{"name":"Tunisia","id":"TN"},{"name":"Turkey","id":"TR"},{"name":"Turkmenistan","id":"TM"},{"name":"Turks and Caicos Islands","id":"TC"}, {"name":"Tuvalu","id":"TV"},{"name":"U.S. Virgin Islands","id":"VI"},{"name":"Uganda","id":"UG"},{"name":"Ukraine","id":"UA"},{"name":"United Arab Emirates","id":"AE"},{"name":"United Kingdom","id":"GB"},{"name":"United States","id":"US"},{"name":"Uruguay","id":"UY"},{"name":"Uzbekistan","id":"UZ"},{"name":"Vanuatu","id":"VU"},{"name":"Vatican","id":"VA"},{"name":"Venezuela","id":"VE"},{"name":"Vietnam","id":"VN"},{"name":"Wallis and Futuna","id":"WF"},{"name":"Yemen","id":"YE"},{"name":"Zambia","id":"ZM"},{"name":"Zimbabwe","id":"ZW"}]
+listcountries = [{"name":"Spain","id":"ES"}]
 
 # ACCEPT COOKIES
-cookies = driver.find_element(By.XPATH, '/html/body/div[1]/div/span[2]/a[2]')
-cookies.click()
+driver.find_elements_by_xpath('/html/body/div[1]/div/span[2]/a[2]')[0].click()
 
-# SET FILTER
-time.sleep(5)
-driver.execute_script('document.getElementById("select_value_label_0").firstChild.innerHTML="France"')
-driver.execute_script('document.getElementById("select_value_label_2").firstChild.innerHTML="Spain"')
-driver.execute_script('document.getElementById("select_value_label_4").firstChild.innerHTML="International"')
-driver.execute_script('document.getElementById("select_value_label_5").firstChild.innerHTML="Air"')
+driver.find_element_by_xpath('//*[@id="select_22"]').click()
+sleep(2) 
 
-# SUBMIT FILTER
-#submit_btn = driver.find_element(By.XPATH, '/html/body/div[1]/div[6]/div/div/div[2]/div[2]/button')
-#submit_btn.click()
+a = driver.find_element_by_xpath('//*[@id="select_listbox_24"]')
+b= a.get_attribute("outerHTML")
+soup = BeautifulSoup(b, 'lxml')
+tag = soup.find_all('md-option')
+
+for x in range(len(tag)):
+    if 'Domestic' in str(tag[x].text):
+        if 'true' in tag[x].get('aria-selected'):
+            driver.find_element_by_xpath('//*[@id="'+ tag[x].get('id') +'"]').click
+    elif 'International' in str(tag[x].text):
+        if 'false' in tag[x].get('aria-selected'):
+            driver.find_element_by_xpath('//*[@id="'+ tag[x].get('id') +'"]').click
+
+    sleep(3)
+    webdriver.ActionChains(driver).send_keys(Keys.ESCAPE).perform()
 
 
-'''
-# GET ELEMENTS
-time.sleep(5)
-origin = driver.find_element(By.XPATH, '/html/body/div[1]/div[6]/div/div/div[1]/div[1]/md-content[1]/md-select/md-select-value/span[1]')
-destination = driver.find_element(By.XPATH, '/html/body/div[1]/div[6]/div/div/div[1]/div[2]/md-content[1]/md-select/md-select-value/span[1]')
-trip_type = driver.find_element(By.XPATH, '/html/body/div[1]/div[6]/div/div/div[2]/md-content[1]/md-select/md-select-value/span[1]')
-demand_category = driver.find_element(By.XPATH, '/html/body/div[1]/div[6]/div/div/div[2]/md-content[2]/md-select/md-select-value/span[1]')
 
-driver.execute_script('arguments[0].innerTEXT = "spain";', origin)
-driver.execute_script('arguments[0].innerTEXT = "France";', destination)
-driver.execute_script('arguments[0].innerTEXT = "International";', trip_type)
-driver.execute_script('arguments[0].innerTEXT = "Air";', demand_category)
-'''
+
+# Submit
+driver.find_element_by_xpath('/html/body/div[1]/div[6]/div/div/div[2]/div[2]/button').click()
+sleep(5)
 
